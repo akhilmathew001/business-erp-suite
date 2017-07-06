f90b1cf4813015c343865f140953b416a71628c3
Akhil Mathew <akhilmakm@gmail.com>
2017-07-06 23:15:24 +0530

    url bug fix

--- base/forms/CustomerForm.py
+++ base/forms/CustomerForm.py
@@ -9,16 +9,16 @@ from base.models.company import Company, State, Country
 
 class CustomerForm(forms.Form):
     
-    name = forms.CharField(label='Customer',max_length=250, required=True)
-    image = forms.ImageField(label='Image')
+    name = forms.CharField(label='Customer',max_length=250, required=True, widget=forms.TextInput)
+    image = forms.ImageField(label='Image', widget=forms.FileInput)
     is_company = forms.BooleanField(label='Is comapany')
     company = forms.ModelChoiceField(queryset=Company.objects.all(), empty_label='Select company', label='Company')
     mobile = forms.IntegerField(label='Mobile')
-    email = forms.EmailField(label='Email')
+    email = forms.EmailField(label='Email', widget=forms.EmailInput)
     street = forms.CharField(label='Lane',max_length=300)
     street2 = forms.CharField(label='Street',max_length=300)
     city = forms.CharField(label='City',max_length=250)
-    state = forms.ModelChoiceField(queryset=State.objects.all(), empty_label='Select state', label='State')
+    state = forms.ModelChoiceField(queryset=State.objects.all(), empty_label='Select state', label='State', widget=forms.Select)
     country = forms.ModelChoiceField(queryset=Country.objects.all(), empty_label='Select country' ,label='Country')
     zip = forms.IntegerField(label='Zip Code')
     note = forms.CharField(label='Internal Note',widget=forms.Textarea)
